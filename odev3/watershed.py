1cd60a62f6c28436d6bec32e99770b2962eb7c37
Furkan Uzun <fuzun096@gmail.com>
2025-04-08 11:29:13 +0300

    update

--- odev3/watershed.py
+++ odev3/watershed.py
@@ -15,14 +15,14 @@ _,binary_image = cv2.threshold(gray_image,50,255,cv2.THRESH_BINARY)
 kernel = np.ones((3,3),np.uint8)
 opening = cv2.morphologyEx(binary_image,cv2.MORPH_OPEN,kernel,iterations=5)
 
-contours,hierarchy = cv2.findContours(opening,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+# contours,hierarchy = cv2.findContours(opening,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
-for cnt in contours:
-    if cv2.contourArea(cnt)>300:
-        (x,y),r = cv2.minEnclosingCircle(cnt)
-        center = (int(x),int(y)) 
-        r =  int(r)
-        cv2.drawContours(image,[cnt],-1,(0,255,0),2)
+# for cnt in contours:
+#     if cv2.contourArea(cnt)>300:
+#         (x,y),r = cv2.minEnclosingCircle(cnt)
+#         center = (int(x),int(y)) 
+#         r =  int(r)
+#         cv2.drawContours(image,[cnt],-1,(0,255,0),2)
 
 
 sure_bg = cv2.dilate(opening,kernel,iterations=2)
@@ -46,13 +46,13 @@ num_objects = len(np.unique(markers)) - 2  # Arka plan hariç tutulur
 cv2.putText(image, f"Para Sayisi: {num_objects}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 
-# circles = cv2.HoughCircles(gray_image,cv2.HOUGH_GRADIENT,1,image.shape[0]/4,param1 = 145,param2 = 38,minRadius=10,maxRadius=120)
+circles = cv2.HoughCircles(gray_image,cv2.HOUGH_GRADIENT,1,image.shape[0]/4,param1 = 145,param2 = 38,minRadius=10,maxRadius=120)
 
 
-# if circles is not None:
-#     circles = np.uint16(np.around(circles))
-#     for i in circles[0, :]:
-#         cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)  # Dış daire
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)  # Dış daire
 
 
 
